47dfa8d7cd298f86a9f5e465644f5c507fb131f1
mir <mehraj@madrosc.com>
2025-11-14 08:36:42 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
- import { ThemeProvider } from './src/contexts/ThemeContext'
-   import { ToastProvider } from './src/contexts/ToastContext'
-   import { AppProvider } from './src/contexts/AppContext'
+import { ThemeProvider } from './src/contexts/ThemeContext';
+import { ToastProvider } from './src/contexts/ToastContext';
+import { AppProvider } from './src/contexts/AppContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
